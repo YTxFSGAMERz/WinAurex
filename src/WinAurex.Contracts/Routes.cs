791267562259f30c3448121771cb393c8156fe36
Farhan <94067297+YTxFSGAMERz@users.noreply.github.com>
2026-07-11 20:11:55 +0530

    Add/Update Routes.cs

---
+++ src/WinAurex.Contracts/Routes.cs
@@ -0,0 +1,9 @@
+namespace WinAurex.Contracts
+{
+    public static class Routes
+    {
+        public static readonly NavigationRoute Dashboard = new("dashboard");
+        public static readonly NavigationRoute Settings = new("settings");
+        public static readonly NavigationRoute Profiles = new("profiles");
+    }
+}
