32f651c5e2a6d0834a1db32ddb8fc2e55c210e7e
Farhan <94067297+YTxFSGAMERz@users.noreply.github.com>
2026-07-12 23:18:23 +0530

    Commit Tweaks/Explorer/Disable_Recent_Frequent.ps1

--- Tweaks/Explorer/Disable_Recent_Frequent.ps1
+++ Tweaks/Explorer/Disable_Recent_Frequent.ps1
@@ -12,9 +12,9 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 }
 
 $HelpersDir = Join-Path -Path $PSScriptRoot -ChildPath "..\..\Core\Helpers"
-Import-Module (Join-Path -Path $HelpersDir -ChildPath "Logging.psm1") -Force
+# Import-Module (Join-Path -Path $HelpersDir -ChildPath "Logging.psm1") -Force
 
-Write-FrameworkLog -ModuleName "Explorer" -Action "Initiating Recent/Frequent Tracking Cleanup"
+# Write-FrameworkLog -ModuleName "Explorer" -Action "Initiating Recent/Frequent Tracking Cleanup"
 
 $RegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer"
 
@@ -22,13 +22,13 @@ $RegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer"
 $RecentVal = (Get-ItemProperty -Path $RegistryPath -Name "ShowRecent" -ErrorAction SilentlyContinue).ShowRecent
 if ($null -eq $RecentVal) { $RecentVal = "Not_Set" }
 Set-ItemProperty -Path $RegistryPath -Name "ShowRecent" -Value 0 -Type DWord -Force
-Write-FrameworkLog -ModuleName "Explorer" -Action "Disable Show Recent Files" -OldValue $RecentVal -NewValue "0"
+# Write-FrameworkLog -ModuleName "Explorer" -Action "Disable Show Recent Files" -OldValue $RecentVal -NewValue "0"
 
 # Setting 2: Show Frequent Folders (0 = Hide, 1 = Show)
 $FrequentVal = (Get-ItemProperty -Path $RegistryPath -Name "ShowFrequent" -ErrorAction SilentlyContinue).ShowFrequent
 if ($null -eq $FrequentVal) { $FrequentVal = "Not_Set" }
 Set-ItemProperty -Path $RegistryPath -Name "ShowFrequent" -Value 0 -Type DWord -Force
-Write-FrameworkLog -ModuleName "Explorer" -Action "Disable Show Frequent Folders" -OldValue $FrequentVal -NewValue "0"
+# Write-FrameworkLog -ModuleName "Explorer" -Action "Disable Show Frequent Folders" -OldValue $FrequentVal -NewValue "0"
 
 Write-Host "`n[SUCCESS] Recent files and Frequent folders have been hidden from Explorer." -ForegroundColor Green
 
